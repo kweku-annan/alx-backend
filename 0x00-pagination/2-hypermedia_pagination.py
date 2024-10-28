c7630a024ff013da8a96ffe6f2e46a68ac607da8
kweku-annan <kwekuannan.dev@gmail.com>
2024-10-28 15:04:02 +0000

    Add changes

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -41,7 +41,8 @@ class Server:
         list_of_rows = dataset[start:end]
         return list_of_rows
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Union[int, List[List]]]:
+    def get_hyper(self, page: int = 1,
+                  page_size: int = 10) -> Dict[str, Union[int, List[List], None]]:
         """Returns the Hypermedia metadate"""
         hypermedia_data = {}
         dataset = self.dataset()
