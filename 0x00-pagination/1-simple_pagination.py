58f02dd8da567097326610da0ab64c62c36f36aa
kweku-annan <kwekuannan.dev@gmail.com>
2024-10-28 04:42:51 +0000

    Made some changes

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,19 +4,19 @@ import csv
 import math
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Takes a page and page_size and returns a tuple of index range"""
-    start = (page - 1) * page_size
-    end = start + page_size
-    return start, end
-
-
 class Server:
     """Server class to paginate a database of popular baby names"""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
+
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Takes a page and page_size and returns a tuple of index range"""
+        start = (page - 1) * page_size
+        end = start + page_size
+        return start, end
 
     def dataset(self) -> List[List]:
         """Cached dataset"""
@@ -34,7 +34,7 @@ class Server:
             (type(page) is int and page > 0) and
             (type(page_size) is int and page_size > 0)
         )
-        start, end = index_range(page, page_size)
+        start, end = self.index_range(page, page_size)
         dataset = self.dataset()
         if start > len(dataset) or end > len(dataset):
             return []
