8cd60be12ed6b3a184dd35fa8f99c17bcf05b81a
kweku-annan <kwekuannan.dev@gmail.com>
2024-10-28 04:30:25 +0000

    Add some changes

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -29,7 +29,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Paginates dataset"""
+        """Paginates dataset and returns result"""
         assert (
             (type(page) is int and page > 0) and
             (type(page_size) is int and page_size > 0)
